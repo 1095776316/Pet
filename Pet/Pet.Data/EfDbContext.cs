7a168f597b5b62dffa9e86188440140ca379a69a
peterli1181 <peterli1181@outlook,com>
2020-08-23 10:25:32 +0800

    11

---
+++ Pet/Pet.Data/EfDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Pet.Data
+{
+    public class EfDbContext : DbContext, IDbContext
+    {
+        public EfDbContext(DbContextOptions<EfDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Pet/Pet.Data/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pet.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pet.Data
+{
+    public interface IDbContext
+    {
+        DbSet<T> Set<T>() where T : BaseEntity;
+
+        int SaveChanges();
+    }
+}
